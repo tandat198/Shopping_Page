57b6ca89157c4699d22878e1291315288782e5ee
Dat Nguyen <tandat198@gmail.com>
2020-02-11 05:37:08 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
